4b539146cd4413e2f41ee5bc7417479318d377b1
Lucie Anglade <lucie@courtbouillon.org>
2021-03-29 14:38:20 +0200

    Fix avoid_versionS

--- doconfly.sh
+++ doconfly.sh
@@ -19,7 +19,7 @@ avoid_versions_pydyf() {
     avoided_versions=""
 }
 
-avoid_version_WeasyPrint() {
+avoid_versions_WeasyPrint() {
     avoided_versions="v47 v46 v45 v44 v43rc2 v43rc1 v43 v0.9 v0.8 v0.7.1 v0.7
     v0.6.1 v0.6 v0.5 v0.42.2 v0.42.1 v0.42 v0.41 v0.40 v0.4 v0.39 v0.38 v0.37
     v0.36 v0.35 v0.34 v0.33 v0.32 v0.31 v0.3.1 v0.30 v0.3 v0.29 v0.28 v0.27
